0255dc05a63ddedf25ccb43a20db717fe70253cd
Administrator <Administrator@EPBYGOMW0027T3>
2014-02-15 22:09:48 +0300

    .

--- src/main/java/org/training/issuetracker/db/util/DBManager.java
+++ src/main/java/org/training/issuetracker/db/util/DBManager.java
@@ -9,25 +9,32 @@ import java.sql.Statement;
 import org.training.issuetracker.db.enums.Role;
 
 public class DBManager {
+	private static final String DRIVER = "org.h2.Driver";
+	private static final String DB_URL = "jdbc:h2:issuetracker";
+	private static final String GUEST = "guest";
+	private static final String GUEST_PASSWORD = "Issue_tracker_guest";
+	private static final String USER = "User";
+	private static final String USER_PASSWORD = "Issue_tracker_user";
+	private static final String ADMINISTRATOR = "administrator";
+	private static final String ADMINISTRATOR_PASSWORD = 
+					"Issue_tracker_administrator";
+	
 	public static Connection getConnection(Role role) 
 			throws ClassNotFoundException, SQLException {
-		Class.forName("org.h2.Driver");
+		Class.forName(DRIVER);
         Connection connection = null;
         switch (role) {
         	case GUEST:
         		connection = DriverManager.
-                		getConnection("jdbc:h2:issuetracker", "guest", 
-                				"Issue_tracker_guest");
+                		getConnection(DB_URL, GUEST, GUEST_PASSWORD);
         		break;
         	case USER: 
         		connection = DriverManager.
-        		getConnection("jdbc:h2:issuetracker", "User", 
-        				"Issue_tracker_user");
+        				getConnection(DB_URL, USER, USER_PASSWORD);
         		break;
         	case ADMINISTRATOR:
         		connection = DriverManager.
-        		getConnection("jdbc:h2:issuetracker", "administrator", 
-        				"Issue_tracker_administrator");
+        			getConnection(DB_URL, ADMINISTRATOR, ADMINISTRATOR_PASSWORD);
         		break;
         }
 		return connection;
